4f61988aced092d9f20044320bbe705e9af194f7
Andrey Doroschenko <andrey.daraschenka@gmail.com>
2024-04-02 15:03:57 +0300

    Added field is_active to the UserCourse model

--- service/database/models/course.py
+++ service/database/models/course.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import (
+    Column, String, Integer, ForeignKey, Boolean, UniqueConstraint
+)
 from sqlalchemy.orm import relationship, backref
 
 from .base import Model
@@ -7,13 +9,20 @@ from .base import Model
 class UserCourse(Model):
     user_id = Column(Integer, ForeignKey("user.id"))
     course_id = Column(Integer, ForeignKey("course.id"))
+    is_active = Column(Boolean, nullable=False, default=False)
+
+    __table_args__ = (
+        UniqueConstraint("user_id", "course_id", name="user_course_unique"),
+    )
 
 
 class Course(Model):
     code = Column(String, nullable=False)
     name = Column(String, nullable=False)
     description = Column(String, nullable=False)
-    users = relationship("User", secondary="user_course", back_populates="courses")
+    users = relationship(
+        "User", secondary="user_course", back_populates="courses"
+    )
     questions = relationship(
         "Question", backref=backref("course", cascade="all,delete"), lazy=True
     )
