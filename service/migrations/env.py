88474edaffe67b1705ac3ef40602b67004064b1f
Andrey Doroschenko <andrey.daraschenka@gmail.com>
2024-07-15 11:50:06 +0300

    Added musl-dev to Dockerfile

--- service/migrations/env.py
+++ service/migrations/env.py
@@ -43,6 +43,7 @@ def run_migrations_offline() -> None:
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        include_schemas=True,
     )
 
     with context.begin_transaction():
@@ -50,7 +51,11 @@ def run_migrations_offline() -> None:
 
 
 def do_run_migrations(connection: Connection) -> None:
-    context.configure(connection=connection, target_metadata=target_metadata)
+    context.configure(
+        connection=connection,
+        target_metadata=target_metadata,
+        include_schemas=True,
+    )
 
     with context.begin_transaction():
         context.run_migrations()
